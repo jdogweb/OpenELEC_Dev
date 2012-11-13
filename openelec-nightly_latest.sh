765cde5024407fc1e17daa4a9682f64e581c69ab
Jules Allen <julesallen@gmail.com>
2012-11-13 22:04:47 +0000

    Revert "Next"
    This reverts commit c2f1e7d0f41c4b4e439d88ae89c630513acc4c5c.

--- openelec-nightly_latest.sh
+++ openelec-nightly_latest.sh
@@ -54,7 +54,7 @@ OPTIND="1"
 
 ###### image location
 
-mode="http://julesallen77.com/tmp/image/"
+mode="http://sources.openelec.tv/tmp/image/"
 
 
 ###### set the temporary file location based on what device we are using...(the rPi does not have enough RAM to download the image to /dev/shm
@@ -116,7 +116,7 @@ do
 			echo "the only build avaliable, is the same build revision you are currently on."
 			echo "Unable to check remote revision number."
 			echo "Please check again later. You may also check manually for yourself here:"
-			echo "http://julesallen77.com/tmp/image/"
+			echo "http://sources.openelec.tv/tmp/image/"
         	echo
         	echo "Exiting Now."
         	rm -rf $temploc
@@ -168,7 +168,7 @@ do
 			echo "the only build avaliable, is the same build revision you are currently on."
 			echo "Unable to display remote revision number."
 			echo "Please check again later. You may also check manually for yourself here:"
-			echo "http://julesallen77.com/tmp/image/"
+			echo "http://sources.openelec.tv/tmp/image/"
         	echo
         	echo "Exiting Now."
 			rm -rf $temploc
@@ -382,7 +382,7 @@ do
         			echo "There are either no available builds for your architecture at this time, or"
 					echo "the only build avaliable, is the same build revision you are currently on."
 					echo "Please check again later. You may also check manually for yourself here:"
-					echo "http://julesallen77.com/tmp/image/"
+					echo "http://sources.openelec.tv/tmp/image/"
         			echo
         			echo "Exiting Now."
         			exit 1
@@ -730,7 +730,7 @@ then
 	echo "http://wiki.openelec.tv/index.php?title=Updating_OpenELEC"
 	echo 
 	echo "Location of development builds:"
-	echo "http://julesallen77.com/tmp/image/"
+	echo "http://sources.openelec.tv/tmp/image/"
 	echo
 	echo "---------------------------------------------------"
 	echo
@@ -801,7 +801,7 @@ then
 				echo "*---| Updating OpenELEC_DEV:"
 				echo -ne "      Please Wait...\033[0K\r"
 				sleep 1
-				curl --silent -fksSL -A "`curl -V | head -1 | awk '{print $2}'`" http://bit.ly/RAMxu6 > `dirname $0`/openelec-nightly_$rsvers.sh &
+				curl --silent -fksSL -A "`curl -V | head -1 | awk '{print $2}'`" http://bit.ly/TOf3qf > `dirname $0`/openelec-nightly_$rsvers.sh &
 				pid=$!
 				spinner $pid
 				unset pid
@@ -945,7 +945,7 @@ then
         echo "There are either no available builds for your architecture at this time, or"
         echo "the only build avaliable, is the same build revision you are currently on."
         echo "Please check again later. You may also check manually for yourself here:"
-        echo "http://julesallen77.com/tmp/image/"
+        echo "http://sources.openelec.tv/tmp/image/"
         echo
         echo "Exiting Now."
         rm -rf $temploc
@@ -997,7 +997,7 @@ then
 	echo "You are currently using an unofficial development build of OpenELEC."
 	echo "This isn't supported, and will yield unusual results if we continue."
 	echo "Your build is a higher revision then whats available on the official"
-	echo "snapshot server, as seen here: http://julesallen77.com/tmp/image/"
+	echo "snapshot server, as seen here: http://sources.openelec.tv/tmp/image/"
 	echo "In order to use this update script, you |*MUST*| be using an official"
 	echo "build, that is avaliable on the public snapshot server."
 	echo
